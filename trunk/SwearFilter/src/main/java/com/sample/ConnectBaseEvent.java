8e3946ce0f952a3722ec7dcb6a564d00c16dd160
a.sabetta <a.sabetta@e768b31c-3acf-4086-bafa-f03597f81cda>
2010-09-08 13:50:55 +0000

    removed obsolete SwearFilter (early experiments with Drools Fusion)

--- trunk/SwearFilter/src/main/java/com/sample/ConnectBaseEvent.java
+++
@@ -1,8 +0,0 @@
-package com.sample;
-
-public interface ConnectBaseEvent <T> {
-	public T getData();
-	public void setData(T t);
-	public Long getTimestamp();
-	public void setTimestamp(Long ts);
-}
